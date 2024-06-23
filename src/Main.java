b0a23db94da8ea0aed66377d25c4c4816011d200
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-06-23 20:08:00 +0300

    HW3-5

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int production = 16;
-        int productionTime = 2;
-        int totalTime;
-        totalTime = 20;
-        int totalProduction = (totalTime / productionTime) * production;
-        System.out.println("За " + totalTime + " минут машина произвела " + totalProduction + " штук бутылок");
-        totalTime = totalTime * 72;
-        int totalProduction2 = (totalTime / productionTime) * production;
-        System.out.println("За " + totalTime + " минут (или сутки) машина произвела " + totalProduction2 + " штук бутылок");
-        totalTime = totalTime * 3;
-        int totalProduction3 = (totalTime / productionTime) * production;
-        System.out.println("За " + totalTime + " минут (или 3 дня) машина произвела " + totalProduction3 + " штук бутылок");
-        totalTime = totalTime * 10;
-        int totalProduction4 = (totalTime / productionTime) * production;
-        System.out.println("За " + totalTime + " минут (или месяц, т е 30 дней) машина произвела " + totalProduction4 + " штук бутылок");
+        int allCans = 120;
+        int white = 2;
+        int brown = 4;
+        int rooms = allCans / (white + brown);
+        int totalWhite = white * rooms;
+        int totalBrown = brown * rooms;
+        System.out.println("В школе, где " + rooms + " классов, нужно " + totalWhite + " банок белой краски и " + totalBrown+ " банок коричневой краски");
     }
 }
