1f59f93a00b4ea00c56ccbafc2baf6af0894be6a
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-06-23 19:34:37 +0300

    HW-2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int number1 = 2024623;
-        byte number2 = 23;
-        short number3 = 623;
-        long number4 = 20240623L;
-        float number5 = 2024.06f;
-        double number6 = 2024.06;
+        int number1 = 27897;
+        byte number2 = 67;
+        short number3 = 569;
+        long number4 = 987678965549L;
+        float number5 = 27.12f;
+        double number6 = 2.786;
         System.out.println("Значение переменной number1 с типом int равно " + number1);
         System.out.println("Значение переменной number2 с типом byte равно " + number2);
         System.out.println("Значение переменной number3 с типом short равно " + number3);
         System.out.println("Значение переменной number4 с типом long равно " + number4);
         System.out.println("Значение переменной number5 с типом float равно " + number5);
         System.out.println("Значение переменной number6 с типом double равно " + number6);
-
     }
 }
