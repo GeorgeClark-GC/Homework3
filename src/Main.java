5e81f4a1fd16b373d41349b1c2e88190970e36d9
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-06-23 20:22:22 +0300

    HW3-6

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int allCans = 120;
-        int white = 2;
-        int brown = 4;
-        int rooms = allCans / (white + brown);
-        int totalWhite = white * rooms;
-        int totalBrown = brown * rooms;
-        System.out.println("В школе, где " + rooms + " классов, нужно " + totalWhite + " банок белой краски и " + totalBrown+ " банок коричневой краски");
+        int numberBananas = 5;
+        int bananaWeight = 80;
+        int numberMilk = 2;
+        int milkWeight = 105;
+        int numberIcecream = 2;
+        int icecreamWeight = 100;
+        int numberEgg = 4;
+        int eggWeight = 70;
+        double totalWeight1 = (numberBananas * bananaWeight) + (numberMilk * milkWeight) + (numberIcecream * icecreamWeight) + (numberEgg * eggWeight);
+        double totalWeight2 = totalWeight1/1000;
+        System.out.println("Вес спортзавтрака в граммах: " + totalWeight1 + "    Вес спортзавтрака в килограммах: " + totalWeight2);
     }
 }
