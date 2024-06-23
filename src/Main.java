26f767fc44235fa8294abcf14115fae75ef2d381
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-06-23 19:41:21 +0300

    HW3-3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int number1 = 27897;
-        byte number2 = 67;
-        short number3 = 569;
-        long number4 = 987678965549L;
-        float number5 = 27.12f;
-        double number6 = 2.786;
-        System.out.println("Значение переменной number1 с типом int равно " + number1);
-        System.out.println("Значение переменной number2 с типом byte равно " + number2);
-        System.out.println("Значение переменной number3 с типом short равно " + number3);
-        System.out.println("Значение переменной number4 с типом long равно " + number4);
-        System.out.println("Значение переменной number5 с типом float равно " + number5);
-        System.out.println("Значение переменной number6 с типом double равно " + number6);
+        int studentsGroup1 = 23;
+        int studentsGroup2 = 27;
+        int studentsGroup3 = 30;
+        int totalPaper = 480;
+        int numberPaper = totalPaper / (studentsGroup1 + studentsGroup2 + studentsGroup3);
+        System.out.println("На каждого ученика рассчитано " + numberPaper + " листов бумаги");
     }
 }
