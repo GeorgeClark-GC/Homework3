74356830805df9839289c94e885910dbc523646b
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-06-23 19:55:50 +0300

    HW3-4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int studentsGroup1 = 23;
-        int studentsGroup2 = 27;
-        int studentsGroup3 = 30;
-        int totalPaper = 480;
-        int numberPaper = totalPaper / (studentsGroup1 + studentsGroup2 + studentsGroup3);
-        System.out.println("На каждого ученика рассчитано " + numberPaper + " листов бумаги");
+        int production = 16;
+        int productionTime = 2;
+        int totalTime;
+        totalTime = 20;
+        int totalProduction = (totalTime / productionTime) * production;
+        System.out.println("За " + totalTime + " минут машина произвела " + totalProduction + " штук бутылок");
+        totalTime = totalTime * 72;
+        int totalProduction2 = (totalTime / productionTime) * production;
+        System.out.println("За " + totalTime + " минут (или сутки) машина произвела " + totalProduction2 + " штук бутылок");
+        totalTime = totalTime * 3;
+        int totalProduction3 = (totalTime / productionTime) * production;
+        System.out.println("За " + totalTime + " минут (или 3 дня) машина произвела " + totalProduction3 + " штук бутылок");
+        totalTime = totalTime * 10;
+        int totalProduction4 = (totalTime / productionTime) * production;
+        System.out.println("За " + totalTime + " минут (или месяц, т е 30 дней) машина произвела " + totalProduction4 + " штук бутылок");
     }
 }
